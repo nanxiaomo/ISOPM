6be4da8f83854a193c0c69a20f4ddeab9936ca2d
wangfei <cnxmhzk@163.com>
2017-03-13 19:18:14 +0900

    登录路由

--- src/main/vuejs/router/index.js
+++ src/main/vuejs/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import home from '../pages/home/home'
+import login from '../pages/login/login'
+import slide from 'components/slide/slide'
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
+      title: '主页',
       component: home
+    },
+    {
+      path: '/slide',
+      name: 'slide',
+      component: slide
+    },
+    {
+      path: '/login',
+      name: 'login',
+      title: '登录',
+      component: login
     }
   ]
 })
-
+export default router
