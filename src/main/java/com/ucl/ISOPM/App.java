a7b50f6e619bf14402c7b9dd14f4e4bcce0d33e4
nanxiaomo <cnxmhzk@163.com>
2017-03-13 18:26:31 +0900

    Revert "update"

---
+++ src/main/java/com/ucl/ISOPM/App.java
@@ -0,0 +1,13 @@
+package com.ucl.ISOPM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
