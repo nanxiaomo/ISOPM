8fdf264bd25aab8cbde8f6a283e5edea3abc7f1a
wangfei <cnxmhzk@163.com>
2017-03-22 16:32:15 +0900

    首页，尾部

---
+++ src/main/vuejs/components/footer/footer.vue
@@ -0,0 +1,23 @@
+<!-- 
+   页面尾部的footer
+   作者: wangfei 
+ -->
+ <template>
+    <div class="footer">
+      作者： 王菲
+    </div>
+  
+ </template>
+ <script type="text/javascript">
+  
+ </script>
+ <style type="text/css">
+  	.footer {
+ 			clear: both;
+ 			position: relative;
+ 			z-index: 10;
+ 			height: 3em;
+ 			margin-top: -3em;
+ 			background-color: #111;
+    }
+ </style>
