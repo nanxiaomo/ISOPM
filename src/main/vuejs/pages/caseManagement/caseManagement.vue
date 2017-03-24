2701a536b58558004bd503ff92c45782961a7765
wangfei <cnxmhzk@163.com>
2017-03-24 15:16:50 +0900

    案件管理

--- src/main/vuejs/pages/caseManagement/caseManagement.vue
+++ src/main/vuejs/pages/caseManagement/caseManagement.vue
@@ -1,27 +1,18 @@
 <template>
   <div class="home">
-    <vheader></vheader>
-    <vslide></vslide>
     <div class="container">
     <vselect></vselect>
     <vtable></vtable>
     </div>
-    <vfooter></vfooter>
   </div>
 </template>
 
 <script type="text/javascript">
-  import vslide from 'components/slide/Slide'
-  import vheader from 'components/header/header'
-  import vfooter from 'components/footer/footer'
   import vtable from 'components/table/casepage'
   import vselect from 'components/select/searchform'
   
   export default {
     components: {
-      vslide,
-      vheader,
-      vfooter,
       vtable,
       vselect
     }
