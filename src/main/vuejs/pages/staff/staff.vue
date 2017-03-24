dfdc712054dfeb7fe56015aac7b3c4a8400c6106
wangfei <cnxmhzk@163.com>
2017-03-24 11:40:00 +0900

    更改路由

--- src/main/vuejs/pages/staff/staff.vue
+++ src/main/vuejs/pages/staff/staff.vue
@@ -1,23 +1,12 @@
 <template>
-  <div class="staff">
-    <vheader></vheader>
-    <vslide></vslide>        
+  <div class="staff">        
     <div class="container">
       <p>人员页</p>
     </div>
-    <vfooter></vfooter>
   </div>
 </template>
 <script type="text/javascript">
-  import vslide from 'components/slide/Slide'
-  import vheader from 'components/header/header'
-  import vfooter from 'components/footer/footer'
   export default {
-    components: {
-      vslide,
-      vheader,
-      vfooter
-    }
   }
 </script>
 <style type="text/css">
--- src/main/vuejs/router/index.js
+++ src/main/vuejs/router/index.js
@@ -4,6 +4,7 @@ import home from 'pages/home/home'
 import login from 'pages/login/login'
 import staff from 'pages/staff/staff'
 import post from 'pages/staff/post'
+import pindex from 'pages/home/index'
 Vue.use(Router)
 
 const router = new Router({
@@ -12,25 +13,31 @@ const router = new Router({
       path: '/',
       name: 'home',
       title: '主页',
-      component: home
+      component: home,
+      children: [
+        {
+          path: '',
+          component: pindex
+        },
+        {
+          path: 'staff',
+          name: 'staff',
+          title: '人员信息管理',
+          component: staff,
+          children: [
+            {
+              path: 'post',
+              component: post
+            }
+          ]
+        }
+      ]
     },
     {
       path: '/login',
       name: 'login',
       title: '登录',
       component: login
-    },
-    {
-      path: '/staff',
-      name: 'staff',
-      title: '人员信息管理',
-      component: staff,
-      children: [
-        {
-          path: 'post',
-          component: post
-        }
-      ]
     }
   ]
 })
